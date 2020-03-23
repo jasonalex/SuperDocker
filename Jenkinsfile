96dd5ec37a21e42a7031240f9520f21977c1e12c
Jason Alex <jason.ebenezer@nutanix.com>
2020-03-23 12:45:36 +0530

    sad

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,10 +3,16 @@ pipeline {
     options {
         skipStagesAfterUnstable()
     }
+    parameters {
+        string(name: 'os', defaultValue: 'alpine', description: 'Choose your Base OS')
+        string(name: 'package', defaultValue: 'python', description: 'Choose your Package')
+    }
+
     stages {
         stage('Creating Docker File') {
             steps {
                 echo "Creating Docker File"
+                sh 'python3 dockerfilegenerator.py -o ${params.os} -p ${params.package}'
             }
         }
         stage('Build Image'){
@@ -22,7 +28,6 @@ pipeline {
             }
             steps {
                 echo "Deploying Image to ECR"
-                sh 'python --version' 
             }
         }
     }
--- dockerfilegenerator.py
+++ dockerfilegenerator.py
@@ -1,12 +1,26 @@
 import argparse
+import CONSTANTS
 from string import Template
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i","--image",type=string, choices=['alpine', 'centos', 'debian'], help="Choose OS alpine/centos/ubuntu")
+parser.add_argument("-o","--os",type=string, choices=['alpine', 'centos', 'debian'], help="Choose OS alpine/centos/ubuntu")
 parser.add_argument("-p","--package",type=string, choices=['python', 'java'], help="Choose Package python/java")
 parser.parse_args()
 
 
-with open('{}-{}.template'.format(args.image, args.package), 'r') as file:
-    template = file.read()
+with open('{}-{}.template'.format(args.os, args.package), 'r') as file:
+    text = file.read()
+
+subd = {
+##Python
+python-gpg-key = CONSTANTS.python-gpg-key
+python-version = CONSTANTS.python-version
+pip-version    = CONSTANTS.pip-version
+pip-url        = CONSTANTS.pip-url
+pip-sha        = CONSTANTS.pip-sha
+alpine-version = CONSTANTS.alpine-version
+}
+
+with open('{}/Dockerfile'.format(args.package), 'rw') as file:
+    file.write(Template(text).safe_substitute(subd))
