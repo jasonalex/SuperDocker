5aeb3ebe8ca16e5e991e95dd43d18c256bff3bb3
Jason Alex <jason.ebenezer@nutanix.com>
2020-03-23 15:58:30 +0530

    zxczx

---
+++ ChooseEcr.py
@@ -0,0 +1,33 @@
+import boto3
+import argparse
+from string import Template
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c","--cx", help="customer account", required=True)
+parser.add_argument("-o","--os", choices=['alpine','debian'], help="Choose OS alpine/debian", required=True)
+parser.add_argument("-p","--package", choices=['python', 'java'], help="Choose Package python/java", required=True)
+args=parser.parse_args()
+
+session = boto3.session.Session()
+ecr = session.client('ecr')
+
+try:
+    response = ecr.describe_repositories(repositoryNames=['{}-{}-{}'.format(args.os,args.package,args.cx)])
+    print('Using Existing Repo')
+except ecr.exceptions.RepositoryNotFoundException as e:
+    print('Creating New Repo')
+    ecr.create_repository(  repositoryName='{}-{}-{}'.format(args.os,args.package,args.cx),
+                            imageTagMutability='MUTABLE',
+                            imageScanningConfiguration={'scanOnPush': True})
+    print('Setting Repo Policies')
+    with open('aws_resources/ecr_policy.json','r') as file:
+        text = file.read()
+    temp = Template(text)
+    ecr.set_repository_policy(
+                                repositoryName='{}-{}-{}'.format(args.os,args.package,args.cx),
+                                policyText=str(temp.substitute(account_id=args.cx)),
+                                force= False
+                            )
+except Exceptions as e:
+    exit(1)
--- Jenkinsfile
+++ Jenkinsfile
@@ -6,8 +6,8 @@ pipeline {
     parameters {
         choice(name: 'os', choices: ['alpine', 'debian'], description: 'Choose Base OS')
         choice(name: 'package', choices: ['python', 'node'], description: 'Choose Package')
+        string(name: 'aws_account', description: ' Destination AWS Account')
     }
-
     stages {
         stage('Creating Docker File') {
             steps {
@@ -18,8 +18,18 @@ pipeline {
         stage('Build Image'){
             steps {
               echo "Building Docker Image"
-
-              sh 'docker build -f build/Dockerfile -t $package-$os:latest .'
+//              sh 'docker build -f build/Dockerfile -t $package-$os:latest .'
+            }
+        }
+        stage('Create ECR Repo') {
+            when {
+              expression {
+                currentBuild.result == null || currentBuild.result == 'SUCCESS'
+              }
+            }
+            steps {
+                echo "Create ECR Repo"
+                sh 'python3 ChooseEcr.py -o $os -p $package -c $aws_account'
             }
         }
         stage('Deploy to ECR') {
